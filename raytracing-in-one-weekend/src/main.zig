5356620a116cd3079f7d8c80949520c9eea4f7d8
jpaquim <joaopaquim@gmail.com>
2022-06-11 13:05:40 +0100

    Log progress to stderr

--- raytracing-in-one-weekend/src/main.zig
+++ raytracing-in-one-weekend/src/main.zig
@@ -5,12 +5,14 @@ pub fn main() anyerror!void {
     const image_height = 256;
 
     const stdout = std.io.getStdOut().writer();
+    const stderr = std.io.getStdErr().writer();
 
     try stdout.print("P3\n{} {}\n255\n", .{ image_width, image_height });
 
     var j: usize = image_height;
     while (j > 0) {
         j -= 1;
+        try stderr.print("\rScanlines remaining: {}", .{j});
         var i: usize = 0;
         while (i < image_width) : (i += 1) {
             const r = @intToFloat(f64, i) / (image_width - 1);
@@ -24,4 +26,6 @@ pub fn main() anyerror!void {
             try stdout.print("{} {} {}\n", .{ ir, ig, ib });
         }
     }
+
+    try stderr.writeAll("\nDone.\n");
 }
