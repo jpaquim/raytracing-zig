563cb3a4be2087b7e4ff12cf12089637cc90d3b4
jpaquim <joaopaquim@gmail.com>
2022-06-16 21:10:56 +0100

    Print running total of pi estimate

--- raytracing-the-rest-of-your-life/src/pi.zig
+++ raytracing-the-rest-of-your-life/src/pi.zig
@@ -3,15 +3,17 @@ const std = @import("std");
 const randomDouble2 = @import("./rtweekend.zig").randomDouble2;
 
 pub fn main() anyerror!void {
-    const N = 1000;
+    const stdout = std.io.getStdOut().writer();
     var inside_circle: usize = 0;
-    var i: usize = 0;
-    while (i < N) : (i += 1) {
+    var runs: usize = 0;
+    while (true) {
+        runs += 1;
         const x = randomDouble2(-1, 1);
         const y = randomDouble2(-1, 1);
         if (x * x + y * y < 1)
             inside_circle += 1;
+
+        if (runs % 100000 == 0)
+            try stdout.print("Estimate of PI = {:.12}\n", .{4 * @intToFloat(f64, inside_circle) / @intToFloat(f64, runs)});
     }
-    const stdout = std.io.getStdOut().writer();
-    try stdout.print("Estimate of PI = {:.12}\n", .{4 * @intToFloat(f64, inside_circle) / N});
 }
