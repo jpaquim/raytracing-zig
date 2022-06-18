3a7efe3b4e64d699d8ec4b100ff7b533aee75a78
jpaquim <joaopaquim@gmail.com>
2022-06-18 12:10:56 +0100

    Perfect importance sampling with analytical integral pdf

--- raytracing-the-rest-of-your-life/src/integrate_x_sq.zig
+++ raytracing-the-rest-of-your-life/src/integrate_x_sq.zig
@@ -1,22 +1,21 @@
 const std = @import("std");
+const pow = std.math.pow;
 const sqrt = std.math.sqrt;
 
-const randomDouble = @import("./rtweekend.zig").randomDouble;
 const randomDouble2 = @import("./rtweekend.zig").randomDouble2;
 
 fn pdf(x: f64) f64 {
-    _ = x;
-    return 0.5;
+    return 3 * x * x / 8;
 }
 
 pub fn main() anyerror!void {
     const stdout = std.io.getStdOut().writer();
-    const N = 1000000;
+    const N = 1;
     var sum: f64 = 0.0;
 
     var i: usize = 0;
     while (i < N) : (i += 1) {
-        const x = randomDouble2(0, 2);
+        const x = pow(f64, randomDouble2(0, 8), 1 / 3);
         sum += x * x / pdf(x);
     }
     try stdout.print("I = {:.12}\n", .{sum / N});
